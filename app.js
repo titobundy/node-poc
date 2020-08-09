2775c61f2dccf290344917ca5fa1f825a759981e
hdiaz.nectia <hdiaz@nectia.com>
2020-08-09 12:35:46 -0400

    NEW: add docker-compose and mongoose

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Mongo Atlas
+// api-user
+// 0GZCvkRqaQ8xDs0w
+mongoose.connect('mongodb+srv://cluster0.sfdso.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    dbName : 'rest-api',
+    user : 'api-user',
+    pass: '0GZCvkRqaQ8xDs0w',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Mongodb connected')
+});
+
+// Mongo Local Docker
+// mongoose.connect('mongodb://localhost:28017/rest-api', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+// .then(() => {
+//     console.log('Mongodb connected')
+// });
 
 const ProductRoute = require('./routes/product.route');
 app.use('/products', ProductRoute);
@@ -10,7 +34,6 @@ app.use((req, res, next) => {
     err.status = 404;
     next(err);
 });
-
 
 // Error Handler
 app.use((err, req, res, next) => {
